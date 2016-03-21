47389de79de42b7b448127ad79cd1055931d9574
Mikael Chudinov <mikael@chudinov.net>
2016-03-21 13:58:42 +0100

    Rename to AspMvc6Multiling

---
+++ src/AspMvc6Multilingual/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AspMvcMultilingual.ViewModels;
+using Microsoft.AspNet.Mvc;
+
+namespace AspMvcMultilingual.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public IActionResult Index()
+        {
+            var home = new Home {Money = (new Random().Next(1, 100000))/10.0};
+            return View(home);
+        }
+    }
+}
